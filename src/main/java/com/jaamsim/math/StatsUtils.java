06c60311597355e0d1baca85b50fba518c81db95
Harvey Harrison <harvey.harrison@gmail.com>
2018-01-27 23:20:11 -0800

    JS: remove trailing whitespace
    Signed-off-by: Harvey Harrison <harvey.harrison@gmail.com>

--- src/main/java/com/jaamsim/math/StatsUtils.java
+++ src/main/java/com/jaamsim/math/StatsUtils.java
@@ -21,7 +21,7 @@ import org.apache.commons.math3.distribution.TDistribution;
 
 /**
  * Stores functions relating to statistics, e.g t-coefficients.
- * 
+ *
  *  @author Michael Bergman
  */
 public class StatsUtils {
@@ -34,18 +34,18 @@ public class StatsUtils {
 		assert(0 <= p);
 		assert(p < 1);
 		assert(1 < n);
-		
+
 		double out = 0f;
 		double degFree = n-1.0;
-			
+
 		p /= 2;
 		// T distribution
 		TDistribution T = new TDistribution(null, degFree, 1e-9);
 		out = -T.inverseCumulativeProbability(p);	// as the distribution is symmetrical
 		return out;
 	}
-		
-	
+
+
 	/**
 	 * To calculate the t-statistic for a given p-value and number of items
 	 * For a 1 tail value
@@ -54,53 +54,53 @@ public class StatsUtils {
 		assert(0 < p);
 		assert(p < 1);
 		assert(1 < n);
-		
+
 		double out = 0f;
 		double degFree = n-1.0;
-			
+
 		// T distribution
 		TDistribution T = new TDistribution(null, degFree, 1e-9);
-		out = T.inverseCumulativeProbability(p);	
+		out = T.inverseCumulativeProbability(p);
 		return out;
 	}
 
-	
+
 	// TODO: maybe change parameter from array to (array)list
 	/**
 	 * Calculates the autocovariance for an array, with all elements having the
 	 * same weight.
-	 * 
+	 *
 	 * @param X the array of the sequence
 	 * @param n the distance between elements of the sequence
 	 * @return the autocovariance
 	 */
 	public static double AutoCovariance(double[] X, int n) {
-		
+
 		assert (n > 0);
 		/*TODO: length check on X to make sure there are enough entries
 		 *		for the given m
 		*/
-		
+
 		double muX0 = 0d;
 		double muXn = 0d;
 		double autocov = 0.0d;
-		
+
 		// calculate the means
 		for(int i = 0; i < X.length - n ; i++) {
 			muX0 += X[i];
 			muXn += X[i+n];
 		}
-		
+
 		muX0 /= X.length - n;
 		muXn /= X.length - n;
-		
-		
+
+
 		for(int i = 0; i < X.length - n ; i++) {
 			autocov += (X[i] - muX0)*(X[i+n] - muXn);
 		}
-		
+
 		autocov /= X.length - n;
-		
+
 		return autocov;
 	}
  }
