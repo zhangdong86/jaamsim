f26d5e9b27ef9224d5ec5da875063448ad811232
Harvey Harrison <harvey.harrison@gmail.com>
2018-01-27 23:39:16 -0800

    JS: fold all stats methods into SequentialSampler
    Contain all the Apache dependencies in one file for now, can refactor
    this out into another class later if more places start relying on it.
    
    Signed-off-by: Harvey Harrison <harvey.harrison@gmail.com>

--- src/main/java/com/jaamsim/RunControlObjects/SequentialSampler.java
+++ src/main/java/com/jaamsim/RunControlObjects/SequentialSampler.java
@@ -17,6 +17,8 @@ package com.jaamsim.RunControlObjects;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import org.apache.commons.math3.distribution.TDistribution;
+
 import com.jaamsim.Samples.SampleListInput;
 import com.jaamsim.Samples.SampleProvider;
 import com.jaamsim.basicsim.Entity;
@@ -29,7 +31,6 @@ import com.jaamsim.input.Keyword;
 import com.jaamsim.input.Output;
 import com.jaamsim.input.UnitTypeListInput;
 import com.jaamsim.input.ValueListInput;
-import com.jaamsim.math.StatsUtils;
 import com.jaamsim.units.DimensionlessUnit;
 import com.jaamsim.units.Unit;
 import com.jaamsim.units.UserSpecifiedUnit;
@@ -301,7 +302,7 @@ public class SequentialSampler extends Entity {
 				stdErr[i] = Math.sqrt( (sumSqVals.get(i) - sumVals.get(i)*sumVals.get(i)/n) / (n-1) / n );
 
 				double p = confIntList.get(i).doubleValue();
-				double t = StatsUtils.tStat2Tail(p, n);
+				double t = SequentialSampler.tStat2Tail(p, n);
 
 				intervalsList.set(i, t*stdErr[i]);
 				withinList.set(i, intervalsList.get(i) < rangesList.get(i).doubleValue());
@@ -316,5 +317,84 @@ public class SequentialSampler extends Entity {
 			result = result && (intervalsList.get(i) < rangesList.get(i).doubleValue());
 		}
 		return result;
+	}
+
+
+	/**
+	 * To calculate the t-statistic for a given p-value and number of items
+	 * For a 2 tail p value, t-test
+	 */
+	public static double tStat2Tail(double p, int n) {
+		assert(0 <= p);
+		assert(p < 1);
+		assert(1 < n);
+
+		double out = 0f;
+		double degFree = n-1.0;
+
+		p /= 2;
+		// T distribution
+		TDistribution T = new TDistribution(null, degFree, 1e-9);
+		out = -T.inverseCumulativeProbability(p);	// as the distribution is symmetrical
+		return out;
+	}
+
+
+	/**
+	 * To calculate the t-statistic for a given p-value and number of items
+	 * For a 1 tail value
+	 */
+	public static double tStat1Tail(double p, int n) {
+		assert(0 < p);
+		assert(p < 1);
+		assert(1 < n);
+
+		double out = 0f;
+		double degFree = n-1.0;
+
+		// T distribution
+		TDistribution T = new TDistribution(null, degFree, 1e-9);
+		out = T.inverseCumulativeProbability(p);
+		return out;
+	}
+
+
+	// TODO: maybe change parameter from array to (array)list
+	/**
+	 * Calculates the autocovariance for an array, with all elements having the
+	 * same weight.
+	 *
+	 * @param X the array of the sequence
+	 * @param n the distance between elements of the sequence
+	 * @return the autocovariance
+	 */
+	public static double AutoCovariance(double[] X, int n) {
+
+		assert (n > 0);
+		/*TODO: length check on X to make sure there are enough entries
+		 *		for the given m
+		*/
+
+		double muX0 = 0d;
+		double muXn = 0d;
+		double autocov = 0.0d;
+
+		// calculate the means
+		for(int i = 0; i < X.length - n ; i++) {
+			muX0 += X[i];
+			muXn += X[i+n];
+		}
+
+		muX0 /= X.length - n;
+		muXn /= X.length - n;
+
+
+		for(int i = 0; i < X.length - n ; i++) {
+			autocov += (X[i] - muX0)*(X[i+n] - muXn);
+		}
+
+		autocov /= X.length - n;
+
+		return autocov;
 	}
 }
